26763642277972e3a1591e549146027605fe3e12
malynda <mchizek@gmail.com>
2014-09-04 19:17:07 -0500

    starting to add real data and tabs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function(){
   var app = angular.module('classPage', []);
 
-  var classes = [{title: "Alegbra 1", time: "2nd Hour", description:"This class focuses on basic principles of algebra..."},{title: "Algebra 2", time: "5th Hour", description: "This class focuses on more advanced algebra topics. Algebra 1 is a prerequisite."}]
+  
 
   app.controller('ClassListController', function(){
     this.classes = classes;
